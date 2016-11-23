3d1a8be92a63aab86b04c23c8f3193249df01121
Sumerano, Michael J <msumerano@gmail.com>
2016-11-23 00:10:33 -0500

    Cleaned up install/uninstall
    Removed redundant uninstall.
    Removed redundant "Install-ChocolateyPath" in installer.

--- src/tools/chocolateyInstall.ps1
+++ src/tools/chocolateyInstall.ps1
@@ -1,14 +1,15 @@
-﻿Install-ChocolateyPackage `
-    -PackageName 'tfpt' `
-    -FileType 'MSI' `
-    -SilentArgs '/qn /norestart ADDDEFAULT=CHECKINPOLICIES,CLI,VSIP,TFPS,TSX_64' `
-    -Url 'https://visualstudiogallery.msdn.microsoft.com/898a828a-af00-42c6-bbb2-530dc7b8f2e1/file/177107/1/tfpt.msi' `
-    -Checksum '503F1677D74556A37D0F5AD08B75DB62430429A2E758E0DE2B5DD4A37A355825' `
-    -ChecksumType 'SHA256'
+﻿$packageName= 'tfpt'
+$url        = 'https://visualstudiogallery.msdn.microsoft.com/898a828a-af00-42c6-bbb2-530dc7b8f2e1/file/177107/1/tfpt.msi'
+$silentArgs = '/Q ADDLOCAL=ALL'
 
-$programs = @{64=${ENV:PROGRAMFILES(X86)};32=$ENV:PROGRAMFILES}[(Get-ProcessorBits)]
-$bin = Join-Path $programs 'Microsoft Team Foundation Server 2015 Power Tools'
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'msi'
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= @(0, 3010, 1641)
+  checksum      = '503F1677D74556A37D0F5AD08B75DB62430429A2E758E0DE2B5DD4A37A355825'
+  checksumType  = 'sha256'
+}
 
-Install-ChocolateyPath `
-    -PathToInstall $bin `
-    -PathType 'Machine'
+Install-ChocolateyPackage @packageArgs
